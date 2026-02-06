841f46d4c7b2c91ea910c366835dac2fed4d0765
Assma <assma.kebbous@epitech.eu>
2026-02-06 09:09:01 +0100

    feat : router

--- backend/router/route.php
+++ backend/router/route.php
@@ -1,31 +1,40 @@
 <?php
-class route{
+class route
+{
     private $path;
     private $callable;
     private $matches = [];
-    private $params = [];
 
     public function __construct($path, $callable)
     {
-        $this->path = trim($path, '/');
+        $this->path = trim($path, "/");
         $this->callable = $callable;
     }
 
-    public function match($url){
-        $url = trim($url, '/');
-        $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path);
+    public function match($url)
+    {
+        $url = trim($url, "/");
+        $path = preg_replace("#:([\w]+)#", "([^/]+)", $this->path);
         $regex = "#^$path$#i";
 
-        if(!preg_match($regex, $url, $matches)){
+        if (!preg_match($regex, $url, $matches)) {
             return false;
-    }
+        }
 
-    array_shift($matches);
-        $this->matches = $matches; 
+        array_shift($matches);
+        $this->matches = $matches;
         return true;
     }
 
-    public function call(){
-    return call_user_func_array($this->callable, $this->matches);
-}
+    public function call()
+    {
+        if (is_string($this->callable)) {
+            $cont = strtok($this->callable, "@");
+            $controller = new $cont();
+            $meth = strtok("@");
+            return call_user_func_array([$controller, $meth], $this->matches);
+        }
+
+        return call_user_func_array($this->callable, $this->matches);
+    }
 }
